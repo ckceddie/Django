165fcdc18464c803486d64a575234c80673cd27a
Eddie Cheng <winland_mis@hotmail.com>
2018-09-10 21:53:05 -0700

    Django : Add / Update / Delete , Validation , URL

--- SemiUsers/apps/Users/models.py
+++ SemiUsers/apps/Users/models.py
@@ -4,23 +4,8 @@ from django.db import models
 
 # Create your models here.
 
-class userManager(models.Manager):
-    def basic_validator(self, postData):
-        errors = {}
-        if len(postData['first_name']) < 5:
-            errors["first_name"] = "First name should be more than 5 characters"
-        if len(postData['email']) < 10:
-            errors["email"] = "Email should be more than 10 characters"
-        return errors
-
-
 class users(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-   # *************************
-    # Connect an instance of BlogManager to our Blog model overwriting
-    # the old hidden objects key with a new one with extra properties!!!
-    objects = userManager()
-    # *************************
--- SemiUsers/apps/Users/views.py
+++ SemiUsers/apps/Users/views.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib import messages
 
 from django.shortcuts import render, HttpResponse, redirect
 from random import randint
 import datetime
 from .models import *
+# errors =[]
 
 
 def index(request):
     context = {
         "users" : users.objects.all(),
     }
-    print "=========================="
-    print context
-    print "=========================="
-
     return render(request,"users/index.html",context)
 
 
@@ -22,43 +20,62 @@ def new(request):
     return render(request, "users/new.html")
 
 def add(request):
+    # ================= [ Add User ]=========================
     users.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email=request.POST['email'])
     return redirect("/users")
 
 def show(request,id):
+    # ================= [ Show User ]=========================
+
     context = {
         "users" : users.objects.filter(id=id),
     }
-    print "=========================="
-    print context
-    print "=========================="
     return render(request,"users/show.html",context)
 
+def validation(request):
+    # ================= [ Validation ]=========================
+
+    errors =[]
+    if len(request.POST['first_name']) < 1:
+        errors.append("First name cannot be emply")
+    if len(request.POST['email']) < 1:
+        errors.append("Email  cannot be emply")
+        context = {
+            "users" : users.objects.filter(id=request.POST['id']),
+            "err" : errors,
+        }
+    return errors
 
 def update(request):
+    # ================= [ Update User ]=========================
 
-    user = users.objects.get(id=request.POST['id'])
-    user.first_name=request.POST['first_name']
-    user.last_name =request.POST['last_name']
-    user.email =request.POST['email']
-    user.save()
-    return redirect("/users")
+    errors=validation(request)
+
+    if len(errors)>0:
+        context = {
+            "users" : users.objects.filter(id=request.POST['id']),
+            "errors" : errors,
+        }
+        return render(request,"users/edit.html",context)
+    else:
+        user = users.objects.get(id=request.POST['id'])
+        user.first_name=request.POST['first_name']
+        user.last_name =request.POST['last_name']
+        user.email =request.POST['email']
+        user.save()
+        return redirect("/users")
 
 
 
 def edit(request,id):
-    print "=========================="
-    print " in EDIT"
-    print "=========================="
+        # ================= [ Edit User ]=========================
     context = {
         "users" : users.objects.filter(id=id),
     }
-    print "=========================="
-    print context
-    print "=========================="
     return render(request,"users/edit.html",context)
 
 def delete(request,id):
+    # ================= [ Delete User ]========================
     getData=users.objects.get(id=id)
     getData.delete()
     return redirect("/users")
